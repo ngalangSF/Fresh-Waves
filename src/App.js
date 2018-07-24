68b3a0cf03929ffe9fef03715e880756e54487a4
ngalang92 <nickgalangsf@gmail.com>
2018-07-24 14:04:54 -0700

    checkpoint_5

--- src/App.js
+++ src/App.js
@@ -13,13 +13,14 @@ class App extends Component {
           <nav>
             <Link to='/'>Landing</Link>
             <Link to='/library'>Library</Link>
+            <Link to='/album'>Album</Link>
           </nav>
           <h1>Bloc Jams</h1>
         </header>
         <main>
           <Route exact path="/" component={Landing} />
           <Route path="/library" component={Library} />
-          <Route path="/album" component={Album} />
+          <Route path="/album/:slug" component={Album} />
         </main>
       </div>
     );
---
+++ src/components/Album.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Album extends Component {
+  render() {
+    return (
+      <section className="album">
+        {this.props.match.params.slug} Album will go here
+      </section>
+    );
+  }
+}
+
+export default Album;
