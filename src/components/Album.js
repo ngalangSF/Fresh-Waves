734e5fc5f27325b51f5941f9e6e067b3b4ac7940
ngalang92 <nickgalangsf@gmail.com>
2018-07-26 21:08:21 -0700

    checkpoint_8

--- src/components/Album.js
+++ src/components/Album.js
@@ -10,9 +10,38 @@ class Album extends Component {
     });
 
     this.state = {
-      album: album
+      album: album,
+      currentSong: album.songs[0],
+      isPlaying: false
     };
 
+    this.audioElement = document.createElement('audio');
+    this.audioElement.src = album.songs[0].audioSrc;
+  }
+
+  play() {
+    this.audioElement.play();
+    this.setState({ isPlaying: true });
+  }
+
+  pause() {
+  this.audioElement.pause();
+  this.setState({ isPlaying: false });
+  }
+
+  setSong(song) {
+  this.audioElement.src = song.audioSrc;
+  this.setState({ currentSong: song });
+  }
+
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+    if (this.state.isPlaying && isSameSong) {
+      this.pause();
+    } else {
+      if (!isSameSong) { this.setSong(song); }
+      this.play();
+    }
   }
 
   render() {
@@ -34,12 +63,12 @@ class Album extends Component {
           </colgroup>
           <tbody>
               {this.state.album.songs.map((song,index) =>
-              <tr key={index}>
+              <tr className="song" key={index} onClick={() => this.handleSongClick(song)}>
                 <td>{(index+1)}</td>
                 <td>{song.title}</td>
                 <td>{song.duration}</td>
-              </tr>)
-            }
+              </tr>
+            )}
           </tbody>
         </table>
       </section>
